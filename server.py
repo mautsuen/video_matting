9fd332c2f254408138a1643758286daa973f1b18
lordolddog87 <lordolddog@gmail.com>
2025-12-28 03:43:43 +0800

    wasm

--- server.py
+++ server.py
@@ -1,16 +1,15 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
+# server.py
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 import mimetypes
 
-# make sure .mjs served as JS
 mimetypes.add_type("text/javascript", ".mjs")
 mimetypes.add_type("application/wasm", ".wasm")
 
 class Handler(SimpleHTTPRequestHandler):
     def end_headers(self):
-        # Cross-origin isolation (needed for wasm threads / SharedArrayBuffer)
         self.send_header("Cross-Origin-Opener-Policy", "same-origin")
         self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
         super().end_headers()
 
 if __name__ == "__main__":
-    ThreadingHTTPServer(("127.0.0.1", 8000), Handler).serve_forever()
+    ThreadingHTTPServer(("0.0.0.0", 8000), Handler).serve_forever()
